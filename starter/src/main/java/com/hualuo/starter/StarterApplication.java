4ebeac231b826562812f5ce1ad720372eb231236
linhualuo <linhualuo@gmail.com>
2017-09-13 10:50:19 +0800

    Set Banner Mode OFF

--- starter/src/main/java/com/hualuo/starter/StarterApplication.java
+++ starter/src/main/java/com/hualuo/starter/StarterApplication.java
@@ -1,7 +1,9 @@
 package com.hualuo.starter;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,11 @@ public class StarterApplication {
     }
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarterApplication.class, args);
+//        SpringApplication app = new SpringApplication(StarterApplication.class);
+//        app.setBannerMode(Banner.Mode.OFF);
+//        app.run(args);
+        new SpringApplicationBuilder(StarterApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
 	}
 }
