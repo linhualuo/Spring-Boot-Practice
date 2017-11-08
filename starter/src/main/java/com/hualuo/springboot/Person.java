28808984ac6709e0b7d9039d04f2d6ae8eebc4d1
linhualuo <linhualuo@gmail.com>
2017-11-09 01:33:43 +0800

    Thymeleaf practice in ch7_2

---
+++ starter/src/main/java/com/hualuo/springboot/Person.java
@@ -0,0 +1,30 @@
+package com.hualuo.springboot;
+
+/**
+ * Bean class Person
+ *
+ * @author Joseph
+ * @create 2017/9/14 12:30
+ */
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
