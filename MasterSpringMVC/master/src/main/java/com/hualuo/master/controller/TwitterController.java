1400398b9a2346ca77ae17ba8ce9812cf47f5b71
linhualuo <linhualuo@gmail.com>
2017-11-21 00:46:03 +0800

    Add stream operation to get tweets list.

--- MasterSpringMVC/master/src/main/java/com/hualuo/master/controller/TwitterController.java
+++ MasterSpringMVC/master/src/main/java/com/hualuo/master/controller/TwitterController.java
@@ -2,10 +2,7 @@ package com.hualuo.master.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.twitter.api.CursoredList;
-import org.springframework.social.twitter.api.SearchResults;
-import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.social.twitter.api.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 控制器
@@ -29,8 +28,11 @@ public class TwitterController {
     @RequestMapping("/")
     public String helloTwitter(@RequestParam(defaultValue = "masterSpringMVC4") String search,Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
-        String text = searchResults.getTweets().get(0).getText();
-        model.addAttribute("message", text);
+        List<String> tweets = searchResults.getTweets()
+                .stream()
+                .map(Tweet::getText)
+                .collect(Collectors.toList());
+        model.addAttribute("tweets", tweets);
         return "resultPage";
     }
 }
