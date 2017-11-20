22674167b23924b0a8be88c805f80909fd600ff3
linhualuo <linhualuo@gmail.com>
2017-11-21 02:00:20 +0800

    Search, redirect control and a simple search text filter.

--- MasterSpringMVC/master/src/main/java/com/hualuo/master/controller/TwitterController.java
+++ MasterSpringMVC/master/src/main/java/com/hualuo/master/controller/TwitterController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,12 +27,39 @@ public class TwitterController {
     @Autowired
     private Twitter twitter;
 
+    /**
+     * 跳转到搜索页面
+     * @return
+     */
     @RequestMapping("/")
+    public String home() {
+        return "searchPage";
+    }
+
+    /**
+     * 跳转到结果页面
+     * @param search
+     * @param model
+     * @return
+     */
+    @RequestMapping("/result")
     public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search,Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
         List<Tweet> tweets = searchResults.getTweets();
         model.addAttribute("tweets", tweets);
         model.addAttribute("search", search);
         return "resultPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String search = request.getParameter("search");
+        //搜索结果不能包含"struts"
+        if (search.toLowerCase().contains("struts")) {
+            redirectAttributes.addFlashAttribute("error", "Try using spring instead.");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
     }
 }
