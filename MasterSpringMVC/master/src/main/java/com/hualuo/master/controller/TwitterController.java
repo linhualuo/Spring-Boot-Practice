e50997f99e357c4d53e480ab97e42d47cf68cbb9
linhualuo <linhualuo@gmail.com>
2017-11-21 00:32:42 +0800

    Fix the last bug with a free vpn.Change back the code.

--- MasterSpringMVC/master/src/main/java/com/hualuo/master/controller/TwitterController.java
+++ MasterSpringMVC/master/src/main/java/com/hualuo/master/controller/TwitterController.java
@@ -21,27 +21,16 @@ import javax.inject.Inject;
  * @create 2017/11/20 16:33
  */
 @Controller
-@RequestMapping("/")
 public class TwitterController {
 
+    @Autowired
     private Twitter twitter;
 
-    private ConnectionRepository connectionRepository;
-
-    @Inject
-    public TwitterController(Twitter twitter, ConnectionRepository connectionRepository) {
-        this.twitter = twitter;
-        this.connectionRepository = connectionRepository;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String helloTwitter(Model model) {
-        if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
-            return "redirect:/connect/twitter";
-        }
-        model.addAttribute(twitter.userOperations().getUserProfile());
-        CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
-        model.addAttribute("friends", friends);
-        return "hello";
+    @RequestMapping("/")
+    public String helloTwitter(@RequestParam(defaultValue = "masterSpringMVC4") String search,Model model) {
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        String text = searchResults.getTweets().get(0).getText();
+        model.addAttribute("message", text);
+        return "resultPage";
     }
 }
