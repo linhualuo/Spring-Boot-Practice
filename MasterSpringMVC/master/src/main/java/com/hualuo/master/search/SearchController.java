2824f20f70f6aba29aabc479a8dde17e26b114d5
linhualuo <linhualuo@gmail.com>
2017-11-23 21:43:30 +0800

    Fix a bug in search url.

--- MasterSpringMVC/master/src/main/java/com/hualuo/master/search/SearchController.java
+++ MasterSpringMVC/master/src/main/java/com/hualuo/master/search/SearchController.java
@@ -3,6 +3,7 @@ package com.hualuo.master.search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,7 +25,7 @@ public class SearchController {
     }
 
     @RequestMapping("/search/{searchType}")
-    public ModelAndView search(@PathVariable String searchType, List<String> keywords) {
+    public ModelAndView search(@PathVariable String searchType, @MatrixVariable List<String> keywords) {
         List<Tweet> tweets = searchService.search(searchType, keywords);
         ModelAndView modelAndView = new ModelAndView("resultPage");
         modelAndView.addObject("tweets", tweets);
