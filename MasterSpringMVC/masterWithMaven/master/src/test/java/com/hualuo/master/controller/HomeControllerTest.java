47cbd152d8a2053606401df86ab2ba7c08c97685
linhualuo <linhualuo@gmail.com>
2017-12-06 02:01:40 +0800

    First junit 4 test.

---
+++ MasterSpringMVC/masterWithMaven/master/src/test/java/com/hualuo/master/controller/HomeControllerTest.java
@@ -0,0 +1,44 @@
+package com.hualuo.master.controller;
+
+import com.hualuo.master.MasterApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * @author Joseph
+ * @create 2017/12/6 1:30
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void should_redirect_to_profile() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/profile"));
+    }
+}
